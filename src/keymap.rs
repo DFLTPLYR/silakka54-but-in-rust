34372dae9695632b9481142640292c376bc7d2d6
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-07-05 22:59:03 +0800

    updated keybinds

--- src/keymap.rs
+++ src/keymap.rs
@@ -8,23 +8,29 @@ pub(crate) const NUM_LAYER: usize = 2;
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         [
+            // left split
             [k!(Escape), k!(Kc1),  k!(Kc2),  k!(Kc3),  k!(Kc4),     k!(Kc5)],
             [k!(Tab),    k!(Q),    k!(W),    k!(E),    k!(R),       k!(T)],
             [k!(LCtrl),  k!(A),    k!(S),    k!(D),    k!(F),       k!(G)],
             [k!(LShift), k!(Z),    k!(X),    k!(C),    k!(V),       k!(B)],
             [a!(No),     a!(No),   a!(No),   mo!(1),   k!(LGui),    k!(Space)],
-            [k!(Kc6),    k!(Kc7),  k!(Kc8),  k!(Kc9),  k!(Kc0),     k!(Minus)],
-            [k!(Y),      k!(U),    k!(I),    k!(O),    k!(P),       k!(Backspace)],
+ 
+            // right split
+            [k!(Kc6),    k!(Kc7),  k!(Kc8),  k!(Kc9),  k!(Kc0),     k!(Backspace)],
+            [k!(Y),      k!(U),    k!(I),    k!(O),    k!(P),       k!(NonusBackslash)],
             [k!(H),      k!(J),    k!(K),    k!(L),    k!(Semicolon), k!(Quote)],
             [k!(N),      k!(M),    k!(Comma), k!(Dot), k!(Slash),   k!(RShift)],
             [k!(Enter),  k!(RCtrl), k!(RAlt), a!(No),  a!(No),      a!(No)],
         ],
         [
+            // left split
             [k!(Grave), k!(F1),  k!(F2),  k!(F3),  k!(F4),  k!(F5)],
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
             [a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent), a!(Transparent)],
             [a!(No), a!(No), a!(No), a!(Transparent), a!(Transparent), a!(Transparent)],
+            
+            // right split
             [k!(F6),   k!(F7),  k!(F8),  k!(F9),  k!(F10), k!(F11)],
             [k!(PageUp), k!(PageDown), k!(Home), k!(End), k!(Delete), k!(F12)],
             [k!(Left), k!(Down), k!(Up), k!(Right), k!(LeftBracket), k!(RightBracket)],
